2a898a45d5f49a06fee11202772ff32453f7344e
jacco <jacco@c7424c16-cddb-4c8f-8308-a74df31739dd>
2013-07-09 18:32:15 +0000

    Added function (and other) comments to log.[ch].

--- libjvs/log.c
+++ libjvs/log.c
@@ -1,5 +1,5 @@
 /*
- * log.c: Description
+ * log.c: Handle logging.
  *
  * Copyright:	(c) 2013 Jacco van Schaik (jacco@jaccovanschaik.net)
  * Version:	$Id$
@@ -23,6 +23,9 @@
 
 #include "log.h"
 
+/*
+ * Out put types.
+ */
 typedef enum {
     OT_UDP,
     OT_TCP,
@@ -32,25 +35,38 @@ typedef enum {
     OT_SYSLOG
 } LOG_OutputType;
 
+/*
+ * An output channel for logging.
+ */
 typedef struct {
     ListNode _node;
     LOG_OutputType type;
     union {
-        FILE *fp;
-        int priority;
+        FILE *fp;       /* OT_FILE, OT_FP */
+        int fd;         /* OT_UDP, OT_TCP, OT_FD */
+        int priority;   /* OT_SYSLOG */
     } u;
 } LOG_Output;
 
+/*
+ * A logger.
+ */
 struct Logger {
     List outputs;
     Buffer scratch;
     int with_date, with_time, with_file, with_func, with_line;
 };
 
+/*
+ * Global variables to store the location of the latest log message.
+ */
 static const char *_log_file;
 static int         _log_line;
 static const char *_log_func;
 
+/*
+ * Create a logging output of <type> for <logger>..
+ */
 static LOG_Output *log_create_output(Logger *logger, LOG_OutputType type)
 {
     LOG_Output *out = calloc(1, sizeof(LOG_Output));
@@ -62,18 +78,27 @@ static LOG_Output *log_create_output(Logger *logger, LOG_OutputType type)
     return out;
 }
 
+/*
+ * Close logging output <out>.
+ */
 static void log_close_output(LOG_Output *out)
 {
     switch(out->type) {
     case OT_UDP:
     case OT_TCP:
+        /* Opened as file descriptors. */
+        close(out->u.fd);
+        break;
     case OT_FILE:
-    case OT_FD:
+        /* Opened as FILE pointers. */
         fclose(out->u.fp);
         break;
     case OT_FP:
+    case OT_FD:
+        /* Not opened by me, so leave them alone. */
         break;
     case OT_SYSLOG:
+        /* Opened using openlog(). */
         closelog();
         break;
     }
@@ -81,6 +106,9 @@ static void log_close_output(LOG_Output *out)
     free(out);
 }
 
+/*
+ * Create a new logger.
+ */
 Logger *logCreate(void)
 {
     Logger *logger = calloc(1, sizeof(Logger));
@@ -90,44 +118,50 @@ Logger *logCreate(void)
     return logger;
 }
 
+/*
+ * Add an output channel that sends messages to a UDP socket on port <port> on host <host>. If the
+ * host could not be found, -1 is returned and the channel is not created.
+ */
 int logToUDP(Logger *logger, const char *host, int port)
 {
     int fd;
-    FILE *fp;
 
     if ((fd = udpConnect(host, port)) < 0)
-        return 1;
-    else if ((fp = fdopen(fd, "w")) == NULL)
-        return 1;
+        return -1;
     else {
         LOG_Output *out = log_create_output(logger, OT_UDP);
-        out->u.fp = fp;
+        out->u.fd = fd;
         return 0;
     }
 }
 
+/*
+ * Add an output channel that sends messages over a TCP connection to port <port> on host <host>. If
+ * no connection could be opened, -1 is returned and the channel is not created.
+ */
 int logToTCP(Logger *logger, const char *host, int port)
 {
     int fd;
-    FILE *fp;
 
     if ((fd = tcpConnect(host, port)) < 0)
-        return 1;
-    else if ((fp = fdopen(fd, "w")) == NULL)
-        return 1;
+        return -1;
     else {
         LOG_Output *out = log_create_output(logger, OT_TCP);
-        out->u.fp = fp;
+        out->u.fd = fd;
         return 0;
     }
 }
 
+/*
+ * Add an output channel to file <file>. If the file could not be opened, -1 is returned and the
+ * channel is not created.
+ */
 int logToFile(Logger *logger, const char *filename)
 {
     FILE *fp;
 
     if ((fp = fopen(filename, "w")) == NULL)
-        return 1;
+        return -1;
     else {
         LOG_Output *out = log_create_output(logger, OT_FILE);
         out->u.fp = fp;
@@ -135,29 +169,34 @@ int logToFile(Logger *logger, const char *filename)
     }
 }
 
+/*
+ * Add an output channel to the previously opened FILE pointer <fp>.
+ */
 int logToFP(Logger *logger, FILE *fp)
 {
     LOG_Output *out = log_create_output(logger, OT_FP);
+
     out->u.fp = fp;
 
     return 0;
 }
 
+/*
+ * Add an output channel to the previously opened file descriptor <fd>.
+ */
 int logToFD(Logger *logger, int fd)
 {
-    FILE *fp;
+    LOG_Output *out = log_create_output(logger, OT_FD);
 
-    if ((fd = dup(fd)) == -1)
-        return 1;
-    else if ((fp = fdopen(fd, "w")) == NULL)
-        return 1;
-    else {
-        LOG_Output *out = log_create_output(logger, OT_FD);
-        out->u.fp = fp;
-        return 0;
-    }
+    out->u.fd = fd;
+
+    return 0;
 }
 
+/*
+ * Add an output channel to the syslog facility using the given parameters (see openlog(3) for the
+ * meaning of these parameters).
+ */
 int logToSyslog(Logger *logger, const char *ident, int option, int facility, int priority)
 {
     LOG_Output *out = log_create_output(logger, OT_SYSLOG);
@@ -169,6 +208,19 @@ int logToSyslog(Logger *logger, const char *ident, int option, int facility, int
     return 0;
 }
 
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) a date of the form YYYY-MM-DD in output
+ * messages. The default is FALSE.
+ */
+void logWithDate(Logger *logger, int on)
+{
+    logger->with_date = on;
+}
+
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) a timestamp of the form HH:MM:SS to output
+ * messages. <precision> is the number of sub-second digits to show. The default is FALSE.
+ */
 void logWithTime(Logger *logger, int on, int precision)
 {
     if (!on) {
@@ -179,26 +231,37 @@ void logWithTime(Logger *logger, int on, int precision)
     }
 }
 
-void logWithDate(Logger *logger, int on)
-{
-    logger->with_date = on;
-}
-
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) the name of the file from which the
+ * logWrite function was called in log messages. The default is FALSE.
+ */
 void logWithFile(Logger *logger, int on)
 {
     logger->with_file = on;
 }
 
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) the name of the function that called the
+ * logWrite function. The default is FALSE.
+ */
 void logWithFunction(Logger *logger, int on)
 {
     logger->with_func = on;
 }
 
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) the line number of the call to the logWrite
+ * function. The default is FALSE.
+ */
 void logWithLine(Logger *logger, int on)
 {
     logger->with_line = on;
 }
 
+/*
+ * Set _log_file, _log_line and _log_func to the given location. Called as part of the logWrite
+ * macro, not to be called on its own.
+ */
 void _logSetLocation(const char *file, int line, const char *func)
 {
     _log_file = file;
@@ -206,6 +269,10 @@ void _logSetLocation(const char *file, int line, const char *func)
     _log_func = func;
 }
 
+/*
+ * Send out a logging message using <fmt> and the subsequent parameters through <logger>. Called as
+ * part of the logWrite macro, not to be called on its own.
+ */
 void _logWrite(Logger *logger, const char *fmt, ...)
 {
     struct timeval tv;
@@ -260,9 +327,11 @@ void _logWrite(Logger *logger, const char *fmt, ...)
         switch(out->type) {
         case OT_UDP:
         case OT_TCP:
+        case OT_FD:
+            tcpWrite(out->u.fd, bufGet(&logger->scratch), bufLen(&logger->scratch));
+            break;
         case OT_FILE:
         case OT_FP:
-        case OT_FD:
             fwrite(bufGet(&logger->scratch), bufLen(&logger->scratch), 1, out->u.fp);
             fflush(out->u.fp);
             break;
@@ -273,6 +342,9 @@ void _logWrite(Logger *logger, const char *fmt, ...)
     }
 }
 
+/*
+ * Close logger <logger>.
+ */
 void logClose(Logger *logger)
 {
     LOG_Output *out;
--- libjvs/log.h
+++ libjvs/log.h
@@ -17,34 +17,90 @@ typedef struct Logger Logger;
 
 #define logWrite (_logSetLocation(__FILE__, __LINE__, __func__), _logWrite)
 
+/*
+ * Create a new logger.
+ */
 Logger *logCreate(void);
 
+/*
+ * Add an output channel that sends messages to a UDP socket on port <port> on host <host>. If the
+ * host could not be found, -1 is returned and the channel is not created.
+ */
 int logToUDP(Logger *logger, const char *host, int port);
 
+/*
+ * Add an output channel that sends messages over a TCP connection to port <port> on host <host>. If
+ * no connection could be opened, -1 is returned and the channel is not created.
+ */
 int logToTCP(Logger *logger, const char *host, int port);
 
+/*
+ * Add an output channel to file <file>. If the file could not be opened, -1 is returned and the
+ * channel is not created.
+ */
 int logToFile(Logger *logger, const char *filename);
 
+/*
+ * Add an output channel to the previously opened FILE pointer <fp>.
+ */
 int logToFP(Logger *logger, FILE *fp);
 
+/*
+ * Add an output channel to the previously opened file descriptor <fd>.
+ */
 int logToFD(Logger *logger, int fd);
 
+/*
+ * Add an output channel to the syslog facility using the given parameters (see openlog(3) for the
+ * meaning of these parameters).
+ */
 int logToSyslog(Logger *logger, const char *ident, int option, int facility, int priority);
 
-void logWithTime(Logger *logger, int on, int precision);
-
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) a date of the form YYYY-MM-DD in output
+ * messages. The default is FALSE.
+ */
 void logWithDate(Logger *logger, int on);
 
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) a timestamp of the form HH:MM:SS to output
+ * messages. <precision> is the number of sub-second digits to show. The default is FALSE.
+ */
+void logWithTime(Logger *logger, int on, int precision);
+
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) the name of the file from which the
+ * logWrite function was called in log messages. The default is FALSE.
+ */
 void logWithFile(Logger *logger, int on);
 
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) the name of the function that called the
+ * logWrite function. The default is FALSE.
+ */
 void logWithFunction(Logger *logger, int on);
 
+/*
+ * Add (if <on> is TRUE) or leave out (if <on> is FALSE) the line number of the call to the logWrite
+ * function. The default is FALSE.
+ */
 void logWithLine(Logger *logger, int on);
 
+/*
+ * Set _log_file, _log_line and _log_func to the given location. Called as part of the logWrite
+ * macro, not to be called on its own.
+ */
 void _logSetLocation(const char *file, int line, const char *func);
 
+/*
+ * Send out a logging message using <fmt> and the subsequent parameters through <logger>. Called as
+ * part of the logWrite macro, not to be called on its own.
+ */
 void _logWrite(Logger *logger, const char *fmt, ...);
 
+/*
+ * Close logger <logger>.
+ */
 void logClose(Logger *logger);
 
 #endif
