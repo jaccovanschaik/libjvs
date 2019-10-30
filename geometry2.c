ab8feb6a954e7d32e06ffb4593eea75599e5ff65
Jacco van Schaik <jacco@jaccovanschaik.net>
2019-10-30 08:56:20 +0100

    Added function comments to geometry2d.[ch]

--- geometry2.c
+++ geometry2.c
@@ -3,7 +3,7 @@
  *
  * Copyright: (c) 2019 Jacco van Schaik (jacco@jaccovanschaik.net)
  * Created:   2019-10-14
- * Version:   $Id: geometry2.c 351 2019-10-14 12:02:28Z jacco $
+ * Version:   $Id: geometry2.c 358 2019-10-25 12:29:14Z jacco $
  *
  * This software is distributed under the terms of the MIT license. See
  * http://www.opensource.org/licenses/mit-license.php for details.
@@ -14,6 +14,11 @@
 
 #include "geometry2.h"
 
+/*                _   _       _
+ * If line <l> is l = s + n * d, this returns the <n> where line <l>
+ * intersects line <m>. The result might be +/- infinity (if the lines are
+ * parallel) or NAN (if they coincide).
+ */
 double g2LineLineIntersect(Line2 l, Line2 m)
 {
     double num = (m.dv.r[1] * (l.pv.r[0] - m.pv.r[0]) - m.dv.r[0] * (l.pv.r[1] - m.pv.r[1]));
@@ -22,6 +27,11 @@ double g2LineLineIntersect(Line2 l, Line2 m)
     return num / den;
 }
 
+/*
+ * Returns the point where line <l> and <m> intersect. The coefficients of the
+ * result may be +/- infinity (if the lines are parallel) or NAN (if they
+ * coincide).
+ */
 Vector2 g2LineLineIntersection(Line2 l, Line2 m)
 {
     double mult = g2LineLineIntersect(l, m);
@@ -29,6 +39,11 @@ Vector2 g2LineLineIntersection(Line2 l, Line2 m)
     return v2Sum(l.pv, v2Scaled(l.dv, mult));
 }
 
+/*                _   _       _
+ * If line <l> is l = s + n * d, this returns the <n> where point <p> is
+ * projected on <l>. The result might be +/- infinity (if the lines are
+ * parallel) or NAN (if they coincide).
+ */
 double g2PointLineProject(Vector2 p, Line2 l)
 {
     double num = l.dv.r[0] * (p.r[0] - l.pv.r[0]) + l.dv.r[1] * (p.r[1] - l.pv.r[1]);
@@ -37,10 +52,13 @@ double g2PointLineProject(Vector2 p, Line2 l)
     return num / den;
 }
 
+/*
+ * Returns the projection of <p> on <l>. The coefficients of the result may be
+ * +/- infinity (if the lines are parallel) or NAN (if they coincide).
+ */
 Vector2 g2PointLineProjection(Vector2 p, Line2 l)
 {
     double mult = g2PointLineProject(p, l);
 
     return v2Sum(l.pv, v2Scaled(l.dv, mult));
 }
-
--- geometry2.h
+++ geometry2.h
@@ -2,11 +2,11 @@
 #define GEOMETRY2_H
 
 /*
- * geometry2.h: XXX
+ * geometry2.h: 2-dimensional geometry.
  *
  * Copyright: (c) 2019 Jacco van Schaik (jacco@jaccovanschaik.net)
  * Created:   2019-10-14
- * Version:   $Id: geometry2.h 351 2019-10-14 12:02:28Z jacco $
+ * Version:   $Id: geometry2.h 358 2019-10-25 12:29:14Z jacco $
  *
  * This software is distributed under the terms of the MIT license. See
  * http://www.opensource.org/licenses/mit-license.php for details.
@@ -29,12 +29,31 @@ typedef struct {
     Vector2 p[];
 } Polygon2;
 
+/*                _   _       _
+ * If line <l> is l = s + n * d, this returns the <n> where line <l>
+ * intersects line <m>. The result might be +/- infinity (if the lines are
+ * parallel) or NAN (if they coincide).
+ */
 double g2LineLineIntersect(Line2 l, Line2 m);
 
+/*
+ * Returns the point where line <l> and <m> intersect. The coefficients of the
+ * result may be +/- infinity (if the lines are parallel) or NAN (if they
+ * coincide).
+ */
 Vector2 g2LineLineIntersection(Line2 l, Line2 m);
 
+/*                _   _       _
+ * If line <l> is l = s + n * d, this returns the <n> where point <p> is
+ * projected on <l>. The result might be +/- infinity (if the lines are
+ * parallel) or NAN (if they coincide).
+ */
 double g2PointLineProject(Vector2 p, Line2 l);
 
+/*
+ * Returns the projection of <p> on <l>. The coefficients of the result may be
+ * +/- infinity (if the lines are parallel) or NAN (if they coincide).
+ */
 Vector2 g2PointLineProjection(Vector2 p, Line2 l);
 
 #endif
